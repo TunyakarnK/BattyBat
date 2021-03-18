a6db1eca9dd7857120fec51a286ec0505d5b118a
TunyakarnK <tunyakarn2544@gmail.com>
2021-03-18 23:11:51 +0700

    Add project files.

---
+++ Bat.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "DEFINITIONS.h"
+#include "Game.h"
+#include <vector>
+class Bat
+{
+public:
+	Bat(GameDataRef data);
+	void Draw();
+	void Animate(float dt);
+	void Update(float dt);
+	void Tap();
+	const sf::Sprite& GetSprite() const;
+private:
+	GameDataRef _data;
+	sf::Sprite _batSprite;
+	std::vector<sf::Texture> _animationFrames;
+	unsigned int _animationIterator;
+	sf::Clock _clock;
+	sf::Clock _movementClock;
+	int _batState;
+	float _rotation;
+};
+
